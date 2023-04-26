e954018cd245b535702bc86a6ffb19ba12faa1c5
Peter Deak <hyper80@gmail.com>
2023-04-26 15:37:51 +0200

    Adds phpattroutes module

---
+++ phpattroutes/README.md
@@ -0,0 +1,39 @@
+# Attribute routes module
+
+This modules lets you to use php attributes (annotations) to define routes for your callback function.
+You can place annotations before your "loc_" prefixed function.
+This annotation will automatically define the routes in the codkep system.
+
+	#[ckpath("start")]
+	function loc_samplepage()
+	{
+	    ob_start();
+	    print "<h2>I am a sample page!</h2>";
+	    print "<div id=\"colorph\">RED</div>";
+	    print lx("Change color","colchange");
+	    print "<br/>";
+	    print l("Go to page with parameter","secondpage/something");
+	    return ob_get_clean();
+	}
+.
+	#[ckpath("colchange"),cktype("ajax")]
+	function loc_colorchanger()
+	{
+	    ajax_add_html('#colorph',"BLUE");
+	}
+	.
+	#[ckpath("secondpage/{str}")]
+	function loc_secondpage()
+	{
+	    par_def('str','text2');
+	    ob_start();
+	    print "<h2>I am a page with parameter!</h2>";
+	    $p = par('str');
+	    print "The parameter value is: $p";
+	    return ob_get_clean();
+	}
+
+
+
+
+
---
+++ phpattroutes/phpattroutes.php
@@ -0,0 +1,78 @@
+<?php
+/*  CodKep php8 attribute defined routes module
+ *
+ *  Module name: phpattroutes
+ *  Written by Peter Deak (C) hyper80@gmail.com , License GPLv2
+ */
+
+function hook_phpattroutes_defineroute()
+{
+    $routes = [];
+    global $sys_data;
+
+    $sys_data->attribute_defined_routes = ccache_get('attribroutes');
+    if($sys_data->attribute_defined_routes != NULL)
+        return $sys_data->attribute_defined_routes;
+
+    $user_defined_functions = get_defined_functions()["user"];
+    foreach($user_defined_functions as $funcname)
+    {
+        if($funcname[0] == 'l' &&
+           isset($funcname[1]) && $funcname[1] == 'o' &&
+           isset($funcname[2]) && $funcname[2] == 'c' &&
+           isset($funcname[3]) && $funcname[3] == '_' )
+        {
+            $refl = new ReflectionFunction($funcname);
+            $att_ckpath = $refl->getAttributes('ckpath');
+
+            if(count($att_ckpath) < 1)
+                continue;
+            $att_ckpath_args = $att_ckpath[0]->getArguments();
+            if(count($att_ckpath_args) < 1)
+                continue;
+
+            $route = [
+                'path' => $att_ckpath_args[0],
+                'callback' => $funcname,
+            ];
+
+            $att_cktype  = $refl->getAttributes('cktype');
+            if(count($att_cktype) > 0)
+            {
+                $att_cktype_args = $att_cktype[0]->getArguments();
+                if(count($att_cktype_args) > 0)
+                    $route['type'] = $att_cktype_args[0];
+            }
+            $att_cktheme = $refl->getAttributes('cktheme');
+            if(count($att_cktheme) > 0)
+            {
+                $att_cktheme_args = $att_cktheme[0]->getArguments();
+                if(count($att_cktheme_args) > 0)
+                    $route['theme'] = $att_cktheme_args[0];
+            }
+            array_push($routes,$route);
+        }
+    }
+
+    run_hook('attribute_defined_routes_generated');
+    ccache_store('attribroutes',$sys_data->attribute_defined_routes,3600);
+    return $routes;
+}
+
+
+function hook_phpattroutes_documentation($section)
+{
+    $docs = [];
+    if($section == "codkep")
+    {
+        $docs[] = [
+            'Phpattroutes' => [
+                'path' => codkep_get_path('phpattroutes','server') . '/phpattroutes.mdoc',
+                'index' => false ,
+                'imagepath' => codkep_get_path('phpattroutes','web') .'/docimages'
+            ]
+        ];
+    }
+    return $docs;
+}
+
